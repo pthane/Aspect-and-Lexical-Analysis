2f60c4e5bb2558389bb9961e21228acf1f71b635
Patrick D. Thane <60477575+pthane@users.noreply.github.com>
2022-03-21 17:13:27 -0400

    Adding data for LAB and Language Acquisition

---
+++ HS Aspect/Scripts/Data Visualization for Heritage.R
@@ -0,0 +1,158 @@
+# Packages
+library(tidyverse)
+library(lme4)
+library(lmerTest)
+
+options(sciphen = 999)
+
+# Preparation of data
+## Load database
+HS_EPT = read_csv("./CSV Files/Heritage/Heritage EPT Preterit Data.csv") %>% 
+  mutate(NewTask = "EPT")
+
+HS_FCT = read_csv("./CSV Files/Heritage/Heritage FCT Preterit Data.csv") %>% 
+  mutate(NewTask = "FCT")
+
+SDC_EPT = read.csv("./CSV Files/Comparison/Comparison EPT Preterit Data.csv") %>% 
+  mutate(NewTask = "EPT")
+
+SDC_FCT = read.csv("./CSV Files/Comparison/Comparison FCT Preterit Data.csv") %>% 
+  mutate(NewTask = "FCT")
+
+
+## Create combined datasets
+Preterit_EPT = rbind(HS_EPT, SDC_EPT)
+Preterit_FCT = rbind(HS_FCT, SDC_FCT)
+Preterit_Aggregate = rbind(Preterit_EPT, Preterit_FCT)
+
+
+# Prepare participant averages for graphs
+## HS production
+HS_EPT_Modified = aggregate(HS_EPT$Response, list(HS_EPT$Participant_ID), FUN = mean, na.rm = TRUE)
+HS_EPT_Modified = HS_EPT_Modified %>% rename(Part_Avg = x)
+HS_EPT_Modified = left_join(HS_EPT, HS_EPT_Modified, by = c("Participant_ID" = "Group.1"))
+
+## HS FCT
+HS_FCT_Modified = aggregate(HS_FCT$Response, list(HS_FCT$Participant_ID), FUN = mean, na.rm = TRUE)
+HS_FCT_Modified = HS_FCT_Modified %>% rename(Part_Avg = x)
+HS_FCT_Modified = left_join(HS_FCT, HS_FCT_Modified, by = c("Participant_ID" = "Group.1"))
+
+
+## Aggregate EPT
+Preterit_EPT_Aggregate = aggregate(Preterit_EPT$Response, list(Preterit_EPT$Participant_ID), FUN = mean, na.rm = TRUE)
+Preterit_EPT_Aggregate = Preterit_EPT_Aggregate %>% rename(Part_Avg = x)
+Preterit_EPT_Aggregate = left_join(Preterit_EPT, Preterit_EPT_Aggregate, by = c("Participant_ID" = "Group.1"))
+
+
+## Aggregate FCT
+Preterit_FCT_Aggregate = aggregate(Preterit_FCT$Response, list(Preterit_FCT$Participant_ID), FUN = mean, na.rm = TRUE)
+Preterit_FCT_Aggregate = Preterit_FCT_Aggregate %>% rename(Part_Avg = x)
+Preterit_FCT_Aggregate = left_join(Preterit_FCT, Preterit_FCT_Aggregate, by = c("Participant_ID" = "Group.1"))
+
+
+## Data from both tasks and groups
+Aggregate_Data = aggregate(Preterit_Aggregate$Response, list(Preterit_Aggregate$Participant_ID), FUN = mean, na.rm = TRUE)
+Aggregate_Data = Aggregate_Data %>% rename(Part_Avg = x)
+Aggregate_Data = left_join(Preterit_Aggregate, Aggregate_Data, by = c("Participant_ID" = "Group.1"))
+
+
+# Prepare verb averages
+## HS only
+HS_EPT_VerbAvg = aggregate(HS_EPT$Response, list(HS_EPT$MainVerb), FUN = mean, na.rm = TRUE)
+HS_EPT_VerbAvg = HS_EPT_VerbAvg %>% rename(Verb_Avg = x)
+HS_EPT_VerbAvg = left_join(HS_EPT, HS_EPT_VerbAvg, by = c("MainVerb" = "Group.1"))
+
+HS_FCT_VerbAvg = aggregate(HS_FCT$Response, list(HS_FCT$MainVerb), FUN = mean, na.rm = TRUE)
+HS_FCT_VerbAvg = HS_FCT_VerbAvg %>% rename(Verb_Avg = x)
+HS_FCT_VerbAvg = left_join(HS_FCT, HS_FCT_VerbAvg, by = c("MainVerb" = "Group.1"))
+
+
+## SDC only
+SDC_EPT_VerbAvg = aggregate(SDC_EPT$Response, list(SDC_EPT$MainVerb), FUN = mean, na.rm = TRUE)
+SDC_EPT_VerbAvg = SDC_EPT_VerbAvg %>% rename(Verb_Avg = x)
+SDC_EPT_VerbAvg = left_join(SDC_EPT, SDC_EPT_VerbAvg, by = c("MainVerb" = "Group.1"))
+
+SDC_FCT_VerbAvg = aggregate(SDC_FCT$Response, list(SDC_FCT$MainVerb), FUN = mean, na.rm = TRUE)
+SDC_FCT_VerbAvg = SDC_FCT_VerbAvg %>% rename(Verb_Avg = x)
+SDC_FCT_VerbAvg = left_join(SDC_FCT, SDC_FCT_VerbAvg, by = c("MainVerb" = "Group.1"))
+
+
+## Aggregate
+HS_Verb_Avg_Master = rbind(HS_EPT_VerbAvg, HS_FCT_VerbAvg)
+Verb_Avg_Master = rbind(HS_EPT_VerbAvg, HS_FCT_VerbAvg, SDC_EPT_VerbAvg, SDC_FCT_VerbAvg)
+
+
+# Plots
+## Individual lexical items
+Aggregate_Data %>%
+  ggplot(aes(x = MainVerb, y = Response, shape = NewTask, color = ExpGroup)) + 
+  geom_hline(yintercept = 0.8, color = "white", size = 2) +
+  stat_summary(fun.data = mean_se,
+               geom = "pointrange", size = 1, 
+               position = position_dodge(width = 0.5)) + 
+  scale_color_brewer(palette = "Set1", name = "") +
+  scale_y_continuous(breaks = seq (0, 1, 0.2),
+                     limits = c(0, 1)) +
+  scale_x_discrete(labels = c("estar", "tener", "haber", "vivir", "gustar", "creer", "faltar", "amar", "doler", "excluir")) +
+  labs(x = "State verb (from most to least frequent)", y = "Proportion of preterit responses",
+       shape = "Task", title = "Average Use of Preterit by Verb and Group") +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold"))
+
+
+## Regularity and task
+Aggregate_Data %>%
+  ggplot(aes(x = Reg_Main, y = Response, color = NewTask)) + 
+  facet_grid(cols = vars(ExpGroup)) +
+  geom_hline(yintercept = 0.8, color = "white", size = 2) + 
+  stat_summary(fun.data = mean_se,
+               geom = "pointrange", size = 1, 
+               position = position_dodge(width = 0.5)) + 
+  scale_color_brewer(palette = "Set1", name = "") +
+  scale_y_continuous(breaks = seq (0, 1, 0.2),
+                     limits = c(0, 1)) +
+  labs(x = "Morphological regularity", y = "Proportion of preterit responses", color = "Task", 
+       title = "Morphological Regularity and Preterit Use") +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold"))
+
+
+## Lexical frequency and task
+Verb_Avg_Master %>%
+  ggplot(aes(x = log(Token_Main_Lemma), y = Verb_Avg, color = NewTask)) + 
+  geom_point() +
+  facet_grid(cols = vars(ExpGroup)) +
+  geom_smooth(method = glm) +
+  scale_y_continuous(breaks = seq (0, 1, 0.2),
+                     limits = c(0, 1)) +
+  labs(x = "Log-transformed lexical frequency in Davies (2016)", y = "Proportion of preterit responses", color = "Task", 
+       title = "Preterit Use by Lexical Frequency and Task") +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold"))
+
+
+## Proficiency and task
+Preterit_Proficiency <- rbind(HS_EPT_Modified, HS_FCT_Modified)
+
+Preterit_Proficiency %>% 
+  ggplot(aes(x = DELE, y = Part_Avg)) + 
+  geom_point() +
+  geom_smooth(method = glm) +
+  facet_grid(cols = vars(NewTask)) +
+  scale_y_continuous(breaks = seq (0, 1, 0.2),
+                     limits = c(0, 1)) +
+  labs(x = "DELE proficiency score", y = "Proportion of preterit responses by participant", color = "Token Frequency", 
+       title = "HS' Proportion of Preterit by Task and Proficiency") +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold"))
+
+
+## Frequency of use and task
+Preterit_FofA <- rbind(HS_EPT_Modified, HS_FCT_Modified)
+
+Preterit_FofA %>% 
+  ggplot(aes(x = FofA, y = Part_Avg)) + 
+  geom_point() +
+  geom_smooth(method = glm) +
+  facet_grid(cols = vars(NewTask)) +
+  scale_y_continuous(breaks = seq (0, 1, 0.2),
+                     limits = c(0, 1)) +
+  labs(x = "Self-reported frequency of use", y = "Proportion of preterit responses by participant", color = "Token Frequency", 
+       title = "HS' Proportion of Preterit by Task and Frequency of Use") +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold"))
